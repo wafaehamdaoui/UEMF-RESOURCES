25b3894d9ce686deac9ad4bb7c121a5859797247
wafaehamdaoui <hamdaouiwafae2000@gmail.com>
2023-01-28 04:32:47 +0330

    comi

--- frontend/src/components/admin.js
+++ frontend/src/components/admin.js
@@ -1,10 +1,10 @@
 import Navbar2 from "./navbar2";
 function Admin(){
     return(
-        <div>
+        <div style={{backgroundColor:"rgba(1, 0, 0, 0.4)"}}>
         <Navbar2 />
         
-        <div style={{backgroundColor:"rgba(1, 0, 0, 0.4)"}}>
+        <div>
             <h1 style={{marginLeft:"40%",marginTop:"5%"}}>Admin page</h1>
             <img src="amin.png" style={{width:"20%",marginLeft:"40%"}}></img>
         </div>
